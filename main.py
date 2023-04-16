adb966698cd2f8b59886bb02f2fcdba581bcde5e
Hilmi Karatatar <karatatar@gmail.com>
2023-04-16 19:42:39 +0300

    use web url

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+from urllib.request import urlopen
 import streamClasses
 
-streams = streamClasses.rawStreamList('diziler.m3u')
 
+Path("m3u/").mkdir(parents=True, exist_ok=True)
+
+with urlopen("https://ft1.ftp.sh/diziler.m3u") as r:
+    s = r.read().decode('utf-8')
+    with open('m3u/diziler.m3u', "w") as fd:
+      fd.write(s)
+
+streams = streamClasses.rawStreamList('m3u/diziler.m3u')
